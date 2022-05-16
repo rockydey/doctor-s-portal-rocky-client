e79924eba61c3dbf420b133769272d8406e7a51b
rockydey <coderrocky25@gmail.com>
2022-05-16 21:06:53 +0600

    Appointment banner section ready

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import footer from '../../assets/images/footer.png';
 
 const Footer = () => {
@@ -7,23 +8,23 @@ const Footer = () => {
             <div className='footer'>
                 <div>
                     <span class="footer-title">Services</span>
-                    <a class="link link-hover">Branding</a>
-                    <a class="link link-hover">Design</a>
-                    <a class="link link-hover">Marketing</a>
-                    <a class="link link-hover">Advertisement</a>
+                    <Link to='/' class="link link-hover">Branding</Link>
+                    <Link to='/' class="link link-hover">Design</Link>
+                    <Link to='/' class="link link-hover">Marketing</Link>
+                    <Link to='/' class="link link-hover">Advertisement</Link>
                 </div>
                 <div>
                     <span class="footer-title">Company</span>
-                    <a class="link link-hover">About us</a>
-                    <a class="link link-hover">Contact</a>
-                    <a class="link link-hover">Jobs</a>
-                    <a class="link link-hover">Press kit</a>
+                    <Link to='/' class="link link-hover">About us</Link>
+                    <Link to='/' class="link link-hover">Contact</Link>
+                    <Link to='/' class="link link-hover">Jobs</Link>
+                    <Link to='/' class="link link-hover">Press kit</Link>
                 </div>
                 <div>
                     <span class="footer-title">Legal</span>
-                    <a class="link link-hover">Terms of use</a>
-                    <a class="link link-hover">Privacy policy</a>
-                    <a class="link link-hover">Cookie policy</a>
+                    <Link to='/' class="link link-hover">Terms of use</Link>
+                    <Link to='/' class="link link-hover">Privacy policy</Link>
+                    <Link to='/' class="link link-hover">Cookie policy</Link>
                 </div>
             </div>
             <div className='text-center mt-10'>
