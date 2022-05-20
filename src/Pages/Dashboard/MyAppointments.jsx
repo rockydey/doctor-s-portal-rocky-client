658fd905b4fcefc4c87eaeae54fbb0db2c658977
rockydey <coderrocky25@gmail.com>
2022-05-20 11:46:13 +0600

    update local server link to live server link

--- src/Pages/Dashboard/MyAppointments.jsx
+++ src/Pages/Dashboard/MyAppointments.jsx
@@ -10,7 +10,7 @@ const MyAppointments = () => {
     const navigate = useNavigate();
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+            fetch(`https://immense-spire-82919.herokuapp.com/booking?patient=${user.email}`, {
                 method: "GET",
                 headers: {
                     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
